6b585a7ccdee6b12657661b77655edf14f147197
Yash Ranadive <yash.ranadive@gmail.com>
2016-05-10 18:42:50 -0700

    Adding options for invoation key, pane size, etc.

--- scripts/tmux-greppage.sh
+++ scripts/tmux-greppage.sh
@@ -1,8 +1,33 @@
 #!/usr/bin/env bash
 
-# Create a new split of size 20 % on the right of the screen
+DEFAULT_COLORIZE=true
+DEFAULT_PANE_SIZE=55
+DEFAULT_SPLIT_DIRECTION="vertical"
 
-# send the grepg command and move focus to the split screen
-tmux command-prompt -p search_string "split-window -h -p 20 'grepg -c evidanary %1 && read'"
-#tmux send-keys -t 2 'grepg '
-#tmux select-pane -t 2
+# Function accepts 2 arguments
+# param 1: the variable to fetch
+# param 2: the default value if not exists
+getTmuxOption () {
+  local option=`tmux show-option -gqv "$1"`
+  option=${option:-$2}
+  echo "$option"
+}
+
+COLORIZE=$(getTmuxOption "@greppage-colorize" $DEFAULT_COLORIZE)
+if [ "$COLORIZE" = true ] ; then
+  COLORIZE="-c"
+else
+  COLORIZE=""
+fi
+
+PANE_SIZE=$(getTmuxOption "@greppage-pane-size" $DEFAULT_PANE_SIZE)
+
+SPLIT_DIRECTION=$(getTmuxOption "@greppage-split-direction" $DEFAULT_SPLIT_DIRECTION)
+if [ "$SPLIT_DIRECTION" == "horizontal" ] ; then
+  SPLIT_DIRECTION="-v"
+else
+  SPLIT_DIRECTION="-h"
+fi
+
+tmux command-prompt -p search_topic "split-window ${SPLIT_DIRECTION} -l ${PANE_SIZE} 'grepg ${COLORIZE} -t %1 && read'"
+
--- tmux-greppage.tmux
+++ tmux-greppage.tmux
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
 CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-tmux bind-key g run-shell "$CURRENT_DIR/scripts/tmux-greppage.sh"
+DEFAULT_GREPPAGE_KEY="g"
+GREPPAGE_KEY=$(tmux show-option -gqv @greppage-key)
+GREPPAGE_KEY=${GREPPAGE_KEY:-$DEFAULT_GREPPAGE_KEY}
+tmux bind-key $GREPPAGE_KEY run-shell "$CURRENT_DIR/scripts/tmux-greppage.sh"
